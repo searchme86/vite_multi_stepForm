c8e26598786ece3f6423ae0af8eebe85d5873c5c
searchme86 <searchme86@gmail.com>
2025-06-12 18:16:14 +0900

    🗑️ 불필요한 파일 삭제

--- src/components/moduleEditor/store/toast/initialToastState.ts
+++
@@ -1,9 +0,0 @@
-import type { ToastItem } from '../shared/commonTypes';
-
-export interface ToastState {
-  toasts: ToastItem[];
-}
-
-export const initialToastState: ToastState = {
-  toasts: [],
-};
