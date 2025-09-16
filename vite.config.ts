33258c847c4262b1ad87c2b6921081457f72ed70
searchme86 <searchme86@gmail.com>
2025-09-16 15:14:14 +0900

    ⚡️ 배포준비

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
-import {defineConfig} from "vite";
-import react from "@vitejs/plugin-react";
-import vitePluginInjectDataLocator from "./plugins/vite-plugin-inject-data-locator";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), vitePluginInjectDataLocator()],
+  plugins: [react()],
+  base: './',
 });
